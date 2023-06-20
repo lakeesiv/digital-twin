7a2317543d394c0ca68d97bccfa84d37290170df
Lakee Sivaraya <lakeesivaraya@gmail.com>
2023-06-20 11:47:42 +0100

    column formatting

--- src/ui/table/table-view-options.tsx
+++ src/ui/table/table-view-options.tsx
@@ -32,7 +32,7 @@ export function DataTableViewOptions<TData>({
           View
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent align="end" className="w-[150px]">
+      <DropdownMenuContent align="end" className="w-[200px]">
         <DropdownMenuLabel>Toggle columns</DropdownMenuLabel>
         <DropdownMenuSeparator />
         {table
