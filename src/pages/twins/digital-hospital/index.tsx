db8f486983d0133c274b0fda3b243d0dd6359ecd
Lakee Sivaraya <lakeesivaraya@gmail.com>
2023-06-22 14:59:50 +0100

    added link

--- src/pages/twins/digital-hospital/index.tsx
+++ src/pages/twins/digital-hospital/index.tsx
@@ -1,5 +1,9 @@
 import Layout from "~/components/layout";
 
 export default function Home() {
-  return <Layout title="Digital Twin"></Layout>;
+  return (
+    <Layout title="Digital Twin">
+      <a href="/twins/digital-hospital/resources">Link</a>
+    </Layout>
+  );
 }
