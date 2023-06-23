6c0a3798f853bfb0690ba5ec2b511b6749582afb
Lakee Sivaraya <lakeesivaraya@gmail.com>
2023-06-23 13:20:38 +0100

    working on index page

--- src/pages/twins/digital-hospital/index.tsx
+++ src/pages/twins/digital-hospital/index.tsx
@@ -1,15 +1,26 @@
 import {
+  Button,
   Card,
   Tab,
   TabGroup,
   TabList,
   TabPanel,
   TabPanels,
+  Text,
+  Title,
 } from "@tremor/react";
 import { Square, SquareStack } from "lucide-react";
 import { useState } from "react";
 import Layout from "~/components/layout";
 import FileUploadMultiple from "~/components/twins/digital-hospital/fileinput";
+import { Input } from "~/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "~/ui/select";
 export default function Home() {
   const [tabIndex, setTabIndex] = useState(0);
 
@@ -44,15 +55,44 @@ export default function Home() {
         </TabList>
         <TabPanels>
           <TabPanel>
-            <Card className="p-40">
+            <Card className="px-8">
+              <Title>Single File Upload</Title>
+
               <FileUploadMultiple />
             </Card>
           </TabPanel>
           <TabPanel>
-            <Card className="p-40" />
+            <Card className="px-8">
+              <Title>Multi File Upload</Title>
+              <FileUploadMultiple />
+            </Card>
           </TabPanel>
         </TabPanels>
       </TabGroup>
+      <Card className="mt-4 flex flex-col space-y-4 p-8">
+        <Title>Configurations</Title>
+        <div className="flex items-center align-middle">
+          <Text className="mr-2">Job Id/Name</Text>
+          <Input className="w-56"></Input>
+        </div>
+        <div className="flex items-center align-middle">
+          <Text className="mr-2">Number of Weeks</Text>
+          <Select>
+            <SelectTrigger className="w-[180px]">
+              <SelectValue placeholder="1 Weeks" />
+            </SelectTrigger>
+            <SelectContent>
+              {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((item) => (
+                <SelectItem key={item} value={String(item)}>
+                  {item} Weeks
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+        </div>
+
+        <Button color="emerald">Run Simulation</Button>
+      </Card>
     </Layout>
   );
 }
