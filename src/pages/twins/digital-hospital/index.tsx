45f73128be50977aa416a5479926f4363446fba4
Lakee Sivaraya <lakeesivaraya@gmail.com>
2023-06-26 14:18:56 +0100

    home page for DH

--- src/pages/twins/digital-hospital/index.tsx
+++ src/pages/twins/digital-hospital/index.tsx
@@ -1,210 +1,53 @@
-import { zodResolver } from "@hookform/resolvers/zod";
-import {
-  Button,
-  Callout,
-  Card,
-  Tab,
-  TabGroup,
-  TabList,
-  TabPanel,
-  TabPanels,
-  Title,
-} from "@tremor/react";
-import { Square, SquareStack } from "lucide-react";
-import { useState } from "react";
-import { useForm } from "react-hook-form";
-import * as z from "zod";
+import { Card, Title, Text, Icon } from "@tremor/react";
+import { Eye, Play } from "lucide-react";
+import Link from "next/link";
+import React from "react";
 import Layout from "~/components/layout";
-import FileUploadMultiple from "~/components/twins/digital-hospital/fileinput";
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "~/ui/form";
-import { Input } from "~/ui/input";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "~/ui/select";
-import { useToast } from "~/ui/use-toast";
-// import { Button } from "~/ui/button";
 
-const noWhitespace = (value: string) => !/\s/.test(value);
-
-const formSchema = z.object({
-  jobId: z
-    .string()
-    .min(3)
-    .max(20)
-    .nonempty()
-    .refine(noWhitespace, "No whitespace allowed in Job ID"),
-  weeks: z.number().int().min(1).max(10),
-  files: z.custom<FileList>().nullable(),
-});
-
-export default function Home() {
-  const [tabIndex, setTabIndex] = useState(0);
-  const toast = useToast();
-
-  const ProcessForm = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      weeks: 1,
-    },
-  });
-
-  const fileList = ProcessForm.watch("files");
-  const setFileList = (files: FileList | null) => {
-    ProcessForm.setValue("files", files);
-  };
-
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    if (!values.files) {
-      toast.toast({
-        title: "No files selected",
-        description: "Please select files to upload",
-        variant: "destructive",
-      });
-    } else {
-      toast.toast({
-        title: "Data",
-        description: (
-          <span className="whitespace-break-spaces font-mono">
-            {JSON.stringify(values, null, 2)}
-          </span>
-        ),
-      });
-    }
-
-    console.log(values);
-  }
-
+const Index = () => {
   return (
-    <Layout title="Digital Twin">
-      <TabGroup
-        index={tabIndex}
-        onIndexChange={(index) => {
-          setFileList(null);
-          setTabIndex(index);
-        }}
-      >
-        <TabList variant="solid">
-          <Tab
-            icon={Square}
-            className={
-              tabIndex === 0
-                ? "border-[1px] border-gray-200 bg-white text-black dark:border-black dark:bg-background dark:text-gray-300"
-                : ""
-            }
-          >
-            Single Simulation
-          </Tab>
-          <Tab
-            icon={SquareStack}
-            className={
-              tabIndex === 1
-                ? "border-[1px] border-gray-200 bg-white text-black dark:border-black dark:bg-background dark:text-gray-300"
-                : ""
-            }
-          >
-            Scenario Analysis
-          </Tab>
-        </TabList>
-        <TabPanels>
-          <TabPanel>
-            <Card className="px-8">
-              <Title>Single File Upload</Title>
-              <FileUploadMultiple
-                fileList={fileList}
-                setFileList={setFileList}
-              />
-            </Card>
-          </TabPanel>
-          <TabPanel>
-            <Card className="px-8">
-              <Title>Multi File Upload</Title>
-              <Callout
-                title="Please name the files in the following format: 1.xlsx, 2.xlsx, 3.xlsx, 4.xlsx, 5.xlsx for the scenario number"
-                className="my-4 bg-blue-100 text-blue-900 dark:bg-blue-950 dark:text-blue-200"
-              ></Callout>
-              <FileUploadMultiple
-                multiple
-                fileList={fileList}
-                setFileList={setFileList}
-              />
-            </Card>
-          </TabPanel>
-        </TabPanels>
-      </TabGroup>
-      <Form {...ProcessForm}>
-        <form
-          onSubmit={(e) => {
-            void ProcessForm.handleSubmit(onSubmit)(e);
-          }}
-        >
-          <Card className="mt-4 flex flex-col space-y-4 p-8">
-            <Title>Configurations</Title>
-            <div className="flex items-start space-x-4 align-top">
-              <FormField
-                control={ProcessForm.control}
-                name="jobId"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Job Id</FormLabel>
-                    <FormControl>
-                      <Input placeholder="" {...field} />
-                    </FormControl>
-                    <FormDescription>Name of the job to be run</FormDescription>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={ProcessForm.control}
-                name="weeks"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Number of Weeks</FormLabel>
-                    <FormControl>
-                      <Select
-                        onValueChange={(value) => {
-                          field.onChange(parseInt(value));
-                        }}
-                        defaultValue={String(field.value)}
-                      >
-                        <SelectTrigger className="w-[180px]">
-                          <SelectValue placeholder="1" />
-                        </SelectTrigger>
-                        <SelectContent>
-                          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((item) => (
-                            <SelectItem key={item} value={String(item)}>
-                              {item}
-                            </SelectItem>
-                          ))}
-                        </SelectContent>
-                      </Select>
-                    </FormControl>
-                    <FormDescription>
-                      Number of Weeks to run the simulation for
-                    </FormDescription>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
+    <Layout title="Digital Hospital">
+      <div className="flex h-full w-full space-x-12  ">
+        <Card className="transform transition-transform duration-300 ease-in-out hover:scale-105">
+          <Link href="/twins/digital-hospital/jobs" className="flex space-x-4">
+            <Icon
+              color="blue"
+              variant="solid"
+              icon={Eye}
+              className="h-12 w-12 items-center justify-center"
+            />
+            <div>
+              <Title>View Jobs</Title>
+              <Text>
+                View the jobs that have been run and the results of the
+                simulations
+              </Text>
             </div>
-            <Button color="emerald" type="submit">
-              Run Simulation
-            </Button>
-          </Card>
-        </form>
-      </Form>
+          </Link>
+        </Card>
+        <Card className="transform transition-transform duration-300 ease-in-out hover:scale-105">
+          <Link
+            href="/twins/digital-hospital/simulate"
+            className="flex space-x-4"
+          >
+            <Icon
+              color="red"
+              variant="solid"
+              icon={Play}
+              className="h-12 w-12 items-center justify-center"
+            />
+            <div>
+              <Title>Run Simulation</Title>
+              <Text>
+                Run a simulation of the Histopathology lab for a given number of
+                weeks
+              </Text>
+            </div>
+          </Link>
+        </Card>
+      </div>
     </Layout>
   );
-}
+};
+
+export default Index;
