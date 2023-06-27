402fb5b6a88e8d5b616dcb9895423abf38999a18
Lakee Sivaraya <lakeesivaraya@gmail.com>
2023-06-27 13:33:13 +0100

    focus ring

--- src/components/charts/download.tsx
+++ src/components/charts/download.tsx
@@ -23,7 +23,7 @@ interface DownloadProps {
 const DownloadButton: React.FC<DownloadProps> = ({ data, type, divId }) => {
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger className=" mx-4 h-[30px]">
+      <DropdownMenuTrigger className="mx-4 h-[30px]  focus:outline-none focus:ring-transparent">
         <Button variant="outline" className="h-[30px]">
           <Download size={16} className="px-0" />
         </Button>
