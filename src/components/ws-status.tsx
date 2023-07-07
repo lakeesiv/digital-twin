3418fe09be523da877514e7ff952037892653fdc
Lakee Sivaraya <lakeesivaraya@gmail.com>
2023-07-07 16:23:22 +0100

    alerts

--- src/components/ws-status.tsx
+++ src/components/ws-status.tsx
@@ -1,14 +1,37 @@
 import React from "react";
 import StatusBage from "./status-badge";
+import { useToast } from "~/ui/use-toast";
 interface WSStatusProps {
-  connectionStatus: string;
+  connectionStatus:
+    | "Connecting"
+    | "Connected"
+    | "Closing"
+    | "Closed"
+    | "Uninstantiated";
   rtConnected: boolean;
+  alert?: boolean;
 }
 
 const WSStatus: React.FC<WSStatusProps> = ({
   connectionStatus,
   rtConnected,
+  alert = true,
 }) => {
+  const toast = useToast();
+
+  React.useEffect(() => {
+    if (alert) {
+      if (connectionStatus === "Closed") {
+        toast.toast({
+          title: "WebSocket Disconnected",
+          description: "WebSocket connection is lost.",
+          variant: "destructive",
+          duration: 3000,
+        });
+      }
+    }
+  }, [connectionStatus]);
+
   return (
     <div className="mb-2 flex flex-row items-center space-x-4">
       <div>
