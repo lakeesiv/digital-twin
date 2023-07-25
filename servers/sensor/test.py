7c771d120ad79a908a68205da9f9deb3df1cca71
Lakee Sivaraya <lakeesivaraya@gmail.com>
2023-07-25 13:21:33 +0000

    working

--- servers/sensor/test.py
+++ servers/sensor/test.py
@@ -1,10 +1,32 @@
-import threading
-import websockets
-import logging
-import uvicorn
-from etl.etl import ETL
-import lib.iolibs as io
 import asyncio
+import logging
+import threading
+from typing import List, Union
+
+import uvicorn
+import websockets
+from fastapi import FastAPI, WebSocket
+
+import lib.iolibs as io
+from etl.etl import ETL
+from etl.decoders.decoder import Decoder
+
+
+class Client:
+    def __init__(self, websocket: WebSocket, acp_config: Union[str, None]):
+        self.websocket = websocket
+        self.acp_list = self._parse_acp_config(acp_config)
+
+    def _parse_acp_config(self, acp_config: Union[str, None]):
+        """Converts a csv string of acp_ids into a list of acp_ids.
+
+        Args:
+            acp_config (str): csv string of acp_ids or single acp_id
+        """
+        if acp_config is None:
+            return None
+
+        return acp_config.split(",")
 
 
 all_synetica_sensors = [
@@ -33,47 +55,71 @@ all_synetica_sensors = [
     "enl-iaqc-085e9a",
     "enl-iaqc-0880a2",
     "enl-iaqc-0862a3"]
+
 # Define a list to keep track of connected clients
-connected_clients = []
+connected_clients: List[WebSocket] = []
 
 
-def handle_message(msg, topic):
+def handle_message(etl, msg, topic):
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
     try:
         # Broadcast the message to all connected clients
         for client in connected_clients:
-            loop.run_until_complete(client.send(msg))
+            msg = Decoder.transform(msg, topic)
+            if (msg):
+                msg = etl.rt_manager.filterParametersFromSensorMessage(
+                    msg)
+                loop.run_until_complete(client.send_json(msg))
     finally:
         loop.close()
 
 
-# Define a WebSocket server function
+# Create a FastAPI app
+app = FastAPI()
+
+# Define WebSocket route
 
 
-def websocket_server():
-    async def _websocket_server(websocket, path):
-        connected_clients.append(websocket)
-        try:
-            async for message in websocket:
-                # You can process incoming messages from clients here if needed
-                pass
-        finally:
-            # Remove the client from the list when they disconnect
-            connected_clients.remove(websocket)
+@app.websocket("/ws/")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    connected_clients.append(websocket)
+    try:
+        while True:
+            # You can process incoming messages from clients here if needed
+            _ = await websocket.receive_text()
+    except BaseException:
+        pass
+    finally:
+        # Remove the client from the list when they disconnect
+        connected_clients.remove(websocket)
 
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    start_server = websockets.serve(_websocket_server, "localhost", 8765)
-    loop.run_until_complete(start_server)
-    loop.run_forever()
 
+@app.websocket("/ws/{acp_id}")
+async def dynamic_websocket_endpoint(websocket: WebSocket, acp_id: str):
+    await websocket.accept()
+    logging.info(f"Adding new client for sensor {acp_id}")
+    connected_clients.append(websocket)
+    try:
+        while True:
+            # You can process incoming messages from clients here if needed
+            _ = await websocket.receive_text()
+    except BaseException:
+        pass
+    finally:
+        # Remove the client from the list when they disconnect
+        connected_clients.remove(websocket)
 
 if __name__ == "__main__":
-    # Start the WebSocket server in a separate thread
+    # Start the FastAPI server in a separate thread
     logging.basicConfig(level=logging.DEBUG)
-    websocket_thread = threading.Thread(target=websocket_server)
-    websocket_thread.start()
+    fastapi_thread = threading.Thread(
+        target=uvicorn.run,
+        args=(app,),
+        kwargs={"host": "localhost", "port": 8000},
+    )
+    fastapi_thread.start()
 
     # Instantiate the ETL class
     config = io.getBasicConfig()
@@ -84,7 +130,7 @@ if __name__ == "__main__":
 
     # Set the handle_message function of the ETL instance to the custom
     # handle_message
-    etl.handle_message = handle_message
+    etl.handle_message = lambda msg, topic: handle_message(etl, msg, topic)
 
     query = {"sensors": []}
     for sensor_id in all_synetica_sensors:
@@ -93,6 +139,6 @@ if __name__ == "__main__":
     # Start the ETL (blocking operation)
     etl.subscribe(query)
 
-    # When the ETL's run method completes, stop the WebSocket server gracefully
+    # When the ETL's run method completes, stop the FastAPI server gracefully
     asyncio.get_event_loop().stop()
-    websocket_thread.join()
+    fastapi_thread.join()
