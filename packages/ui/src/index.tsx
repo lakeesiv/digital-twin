edc7a174112b532d3030d7e54bbd412ce69aab74
Lakee Sivaraya <lakeesivaraya@gmail.com>
2023-07-28 08:51:44 +0000

    hover card

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -8,6 +8,7 @@ export * from "./dialog";
 export * from "./dropdown-menu";
 export * from "./facted-filter-button";
 export * from "./form";
+export * from "./hover-card";
 export * from "./input";
 export * from "./label";
 export * from "./navigation-menu";
