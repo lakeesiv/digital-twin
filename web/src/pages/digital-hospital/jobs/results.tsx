5b155ca2edadf57a9e8e8f3b8bedaa4f0e45c5bd
Lakee Sivaraya <lakeesivaraya@gmail.com>
2023-08-09 09:17:17 +0000

    added some error checking

--- web/src/pages/digital-hospital/jobs/results.tsx
+++ web/src/pages/digital-hospital/jobs/results.tsx
@@ -5,28 +5,47 @@ import { transformApiData } from "~/api/digital-hospital/transform";
 import SingleScenarioResult from "~/components/twins/digital-hospital/single-scenario-result";
 import { GetServerSideProps } from "next";
 import { getScenario } from "~/api/digital-hospital";
+import { SimulationResults } from "~/api/config";
 
 interface Props {
-  results: ReturnType<typeof transformApiData>;
+  results: SimulationResults;
+  error: boolean;
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ query }) => {
   const id = query.id as string;
-  const data = await getScenario(parseInt(id));
+  try {
+    const data = await getScenario(parseInt(id));
 
-  return {
-    props: {
-      results: JSON.parse(JSON.stringify(data)) as ReturnType<
-        typeof transformApiData
-      >,
-    },
-  };
+    return {
+      props: {
+        results: JSON.parse(JSON.stringify(data)) as SimulationResults, // We need to do this because Next.js doesn't support sending objects as props
+        error: false,
+      },
+    };
+  } catch (error) {
+    return {
+      props: {
+        results: null,
+        error: true,
+      },
+    };
+  }
 };
 
-const SingleScenarioPage: React.FC<Props> = ({ results }) => {
+const SingleScenarioPage: React.FC<Props> = ({ results, error }) => {
   return (
     <Layout>
-      <SingleScenarioResult results={results} />
+      {error ? (
+        <div className="mt-8">
+          <p>
+            There was an error fetching the scenario. Please ensure that the
+            Simulation Server is running and ensure the jobId is valid.
+          </p>
+        </div>
+      ) : (
+        <SingleScenarioResult results={results} />
+      )}
     </Layout>
   );
 };
