3c9369ab22ad57c52e19912ddae75b96b73ea696
Lakee Sivaraya <lakeesivaraya@gmail.com>
2023-08-16 12:30:26 +0000

    prod env

---
+++ ecosystem.config.js
@@ -0,0 +1,34 @@
+const prodEnv = {
+  WEB_PORT: 2222,
+  SENSOR_PORT: 7777,
+  DH_PORT: 5000, // Can not be configured on this file
+};
+
+const devEnv = {
+  DH_PORT: 5000,
+  SENSOR_PORT: 8000,
+};
+
+module.exports = {
+  apps: [
+    {
+      name: "web",
+      script: "cd web && PORT=$WEB_PORT SENSOR_PORT=$SENSOR_PORT pnpm start",
+      args: "start",
+      autorestart: false,
+      watch: true,
+      prodEnv: prodEnv,
+      devEnv: devEnv,
+    },
+    {
+      name: "sensor-server",
+      script:
+        "cd servers/sensor && source venv/bin/activate && PORT=$SENSOR_PORT python main.py",
+      args: "start",
+      autorestart: false,
+      watch: true,
+      prodEnv: prodEnv,
+      devEnv: devEnv,
+    },
+  ],
+};
--- web/site.config.ts
+++ web/site.config.ts
@@ -1,5 +1,13 @@
+import config from "../ecosystem.config";
+const prodEnv = config.apps[0].prodEnv;
+const devEnv = config.apps[0].devEnv;
+
+const isProd = process.env.NODE_ENV === "production";
+
+const env = isProd ? prodEnv : devEnv;
+
 export const siteConfig = {
-  DH_API_URL: "http://127.0.0.1:5000",
-  SENSOR_API_URL: "http://localhost:8000",
-  SENSOR_WS_URL: "ws://localhost:8000/ws/", // make sure to include trailing slash
+  DH_API_URL: `http://127.0.0.1:${env.DH_PORT}`,
+  SENSOR_API_URL: `http://localhost:${env.SENSOR_PORT}`,
+  SENSOR_WS_URL: `ws://localhost:${env.SENSOR_PORT}/ws/`, // make sure to include trailing slash
 };
